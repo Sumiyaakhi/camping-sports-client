9df48fe993bfe365788412079ee10df2104d0459
Sumiyaakhi <sumiya.akhi793@gmail.com>
2023-06-09 19:27:27 +0600

    authprovider added

--- src/Routers/Routes.jsx
+++ src/Routers/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import Main from '../Layouts/Main';
 import Login from '../Pages/Login/Login';
+import Registration from '../Pages/Registration/Registration';
 
 const router = createBrowserRouter([
     {
@@ -13,7 +14,8 @@ const router = createBrowserRouter([
           element:<Login></Login>
         },
         {
-          
+          path:'/registration',
+          element:<Registration></Registration>
         }
       ]
     },
